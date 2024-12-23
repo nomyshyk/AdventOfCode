ba6802a3f9202d6642bee97a6edd5953008ccb1f
Alisher_Kasymov <kasiom@gmail.com>
2024-12-23 16:53:04 -0600

    solving 14 Day2

--- src/main/java/kg/com/Day14.java
+++ src/main/java/kg/com/Day14.java
@@ -8,36 +8,22 @@ import java.util.stream.Collectors;
 public class Day14 {
 
     static List<List<String>> inputList = new ArrayList<>();
-    public static Map<String, List<List<Pair<Integer, Integer>>>> cacheOfWallIntervals = new HashMap<>();
-
-    public static void populateCacheForAllSides() {
-        cacheOfWallIntervals.putIfAbsent("WEST", cacheStoneIntervals(inputList));
-
-        List<List<String>> north = rotateMatrix(inputList);
-        cacheOfWallIntervals.putIfAbsent("NORTH", cacheStoneIntervals(north));
-
-        List<List<String>> east = reflectMatrix(inputList);
-        cacheOfWallIntervals.putIfAbsent("EAST", cacheStoneIntervals(east));
-
-        List<List<String>> south = reflectMatrix(north);
-        cacheOfWallIntervals.putIfAbsent("SOUTH", cacheStoneIntervals(south));
-
-        System.out.println(cacheOfWallIntervals);
-    }
-
+    static Map<Integer, Integer> cacheHash = new HashMap<>();
     public static Long executePart1(List<String> strList) {
 
         clear();
         inputList = parse2DList(strList);
 
         long total = 0;
-        //int cnt = 0;
-        List<List<String>> rotatedMatrix = rotateMatrix(inputList);
+        //printMatrix(cntClockRotate(inputList));
+        List<List<String>> rotatedMatrix = cntClockRotate(inputList);
+        //System.out.println("res");
+        //printMatrix(rotatedMatrix);
         for (List<String> list: rotatedMatrix){
-            //total += order(list);
             List<String> s = splitOrderMerge(list);
             total += counter(s);
-            System.out.println(total);
+            //System.out.println(s);
+            //System.out.println(total);
         }
 
         System.out.printf("Total is %s", total);
@@ -50,7 +36,7 @@ public class Day14 {
         long result = 0L;
         for(int i = 0; i < list.size(); i++) {
             if("O".equals(list.get(i))) {
-                result += list.size()-i;
+                result += i+1;
             }
         }
         return result;
@@ -58,62 +44,66 @@ public class Day14 {
     public static Long executePart2(List<String> strList) {
         clear();
         inputList = parse2DList(strList);
-        //populateCacheForAllSides();
-        //List<List<String>> rotatedMatrix = rotateMatrix(inputList);
 
-        List<List<String>> updatedMatrix = rotate(inputList);
-
-        //if(cacheOfWallIntervals.)
-//        for (List<String> list: rotatedMatrix){
-//            updatedMatrix.add(newMatrix(list));
-//        }
+        //printMatrix(clockwiseRotateMatrix(inputList));
+//        //Should be eq
+//        System.out.println("should be eq");
+//        printMatrix(lists);
 //
-//        int hashCode = updatedMatrix.hashCode();
+//        List<List<String>> updatedMatrix = rotate(inputList);
 
-        // should be done at the end
+        int hashOfMatrix = 0;
+        boolean notInCache = false;
+        int rotationN = 0;
+        List<List<String>> updatedMatrix = new ArrayList<>(inputList);
+
+        while (!notInCache) {
+            updatedMatrix = rotate(updatedMatrix);
+            hashOfMatrix = updatedMatrix.hashCode();
+            if(cacheHash.get(hashOfMatrix) != null) {
+                notInCache = true;
+            } else {
+                cacheHash.put(hashOfMatrix, ++rotationN);
+            }
+        }
+
+        System.out.printf("It copied after %s cycles \n", rotationN);
+        int additionalCycles = (1000000000 % rotationN);
+        System.out.printf("Rotate additional %s times \n", additionalCycles);
+
+        for(int i = 0; i < additionalCycles; i++) {
+            updatedMatrix = rotate(updatedMatrix);
+        }
+
         long total = 0;
         for (List<String> list: updatedMatrix){
-            total += order(list);
+            //List<String> s = splitOrderMerge(list);
+            total += counter(list);
+            System.out.println(total);
         }
 
         return total;
     }
 
     static List<List<String>> rotate(List<List<String>> matrix) {
-        //System.out.println(rotateMatrix(matrix));
-
-        // read north
+        List<List<String>> origMatrix = new ArrayList<>(matrix);
         List<List<String>> updatedMatrix = new ArrayList<>();
-        //System.out.println(matrix);
-        for (List<String> list: rotateMatrix(matrix)){
-            updatedMatrix.add(newMatrix(list, false));
+        for (int i = 1; i <= 4; i++) {
+            System.out.printf("prepare rotation #%s \n", i);
+            printMatrix(origMatrix);
+            origMatrix = cntClockRotate(origMatrix);
+            System.out.printf("after rotation #%s \n", i);
+            printMatrix(origMatrix);
+            for (List<String> list: origMatrix){
+                updatedMatrix.add(splitOrderMerge(list));
+            }
+            System.out.printf("after tilt #%s \n", i);
+            printMatrix(updatedMatrix);
+            origMatrix = new ArrayList<>(updatedMatrix);
+            updatedMatrix.clear();
         }
 
-        // read west
-        System.out.println(updatedMatrix);
-//        List<List<String>> north = rotateMatrix(updatedMatrix);
-//        updatedMatrix.clear();
-//        for (List<String> list: north){
-//            updatedMatrix.add(newMatrix(list, false));
-//        }
-//
-//        System.out.println("First rotation");
-//        System.out.println(updatedMatrix);
-
-//        List<List<String>> east = reflectMatrix(inputList);
-//        updatedMatrix.clear();
-//        for (List<String> list: east){
-//            updatedMatrix.add(newMatrix(list));
-//        }
-//
-//
-//        List<List<String>> south = reflectMatrix(north);
-//        updatedMatrix.clear();
-//        for (List<String> list: south){
-//            updatedMatrix.add(newMatrix(list));
-//        }
-
-        return updatedMatrix;
+        return origMatrix;
     }
 
     static List<List<String>> parse2DList(List<String> strList) {
@@ -173,69 +163,8 @@ public class Day14 {
 
     public static String sortStr(String unsorted) {
         return String.join("", Arrays.stream(unsorted.split(""))
-                .sorted(Comparator.reverseOrder())
+                .sorted(Comparator.naturalOrder())
                 .toList());
-    }
-
-    public static List<String> newMatrix(List<String> unorderedList, boolean isReverse) {
-        List<Pair<Integer, Integer>> stonePositions = new ArrayList<>();
-        List<Integer> counts = new ArrayList<>();
-        int lastStonePos = 0;
-        int cnt0 = 0;
-        for (int i = 0; i < unorderedList.size(); i++) {
-            if (unorderedList.get(i).equals("O")){
-                cnt0++;
-            }
-
-            if (unorderedList.get(i).equals("#") || (i == unorderedList.size()-1)) {
-                //System.out.printf("new pair %s and %s%n", lastStonePos, i);
-                stonePositions.add(Pair.of(((lastStonePos==0 && stonePositions.isEmpty()) ? 0 : lastStonePos + 1), i));
-                lastStonePos = i;
-                counts.add(cnt0);
-                cnt0 = 0;
-            }
-        }
-
-        // If empty use all range
-        if (stonePositions.isEmpty()) {
-            stonePositions.add(Pair.of(0, unorderedList.size() - 1));
-            counts.add(0);
-        }
-
-        List<String> newArr = new ArrayList();
-        //long total = 0L;
-        for (int i = 0; i < stonePositions.size(); i++) {
-            newArr.addAll(generateNewLine(stonePositions.get(i), counts.get(i), isReverse));
-        }
-        return newArr;
-    }
-
-//    public List<String> sort(List<String> unordered, boolean isReverse) {
-//        for(int i = 1; i<unordered.size(); i++) {
-//            if(unordered.get(i-1).equals(".") && unordered.get(i).equals("O")){
-//
-//            }
-//        }
-//    }
-
-    public static List<List<Pair<Integer, Integer>>> cacheStoneIntervals(List<List<String>> matrix) {
-        List<List<Pair<Integer, Integer>>> cachedMatrix = new ArrayList<>();
-        for(int ii = 0; ii< matrix.size(); ii++) {
-            List<Pair<Integer, Integer>> stonePositions = new ArrayList<>();
-            int lastStonePos = 0;
-            for(int j =0; j < matrix.get(ii).size(); j++) {
-                if (matrix.get(ii).get(j).equals("#") || (j == matrix.get(ii).size()-1)) {
-                    //System.out.printf("new pair %s and %s%n", lastStonePos, i);
-                    stonePositions.add(Pair.of(((lastStonePos==0 && stonePositions.isEmpty()) ? 0 : lastStonePos + 1), j));
-                    lastStonePos = j;
-                }
-            }
-            if (stonePositions.isEmpty()) {
-                stonePositions.add(Pair.of(0, matrix.get(ii).size() - 1));
-            }
-            cachedMatrix.add(stonePositions);
-        }
-        return cachedMatrix;
     }
 
     //Counts
@@ -248,21 +177,6 @@ public class Day14 {
             total += (totalLength - i);
         }
         return total;
-    }
-
-    public static List<String> generateNewLine(Pair<Integer, Integer> border, int amtOfZeros, boolean isReverse) {
-        List<String> arr = new ArrayList<>();
-
-        for (int i = 0; i < border.getRight() - border.getLeft() + 1; i++) {
-            if(i == 0 || i == (border.getRight() - border.getLeft() + 1)) {
-                arr.add("#");
-            } else if(i <= amtOfZeros) {
-                arr.add("O");
-            } else {
-                arr.add(".");
-            }
-        }
-        return arr;
     }
 
     public static List<List<String>> rotateMatrix(List<List<String>> matrix) {
@@ -302,7 +216,32 @@ public class Day14 {
         return rotatedMatrix;
     }
 
+    public static List<List<String>> cntClockRotate(List<List<String>> matrix) {
+        List<List<String>> rotatedMatrix = new ArrayList<>();
+        for(int i = 0; i < matrix.size(); i++){
+            List<String> newStr = new ArrayList<>();
+            for (int j = 0; j < matrix.get(i).size(); j++) {
+//                String old = matrix.get(i).get(j);
+//                int newI = matrix.get(0).size()-1 - j;
+//                int newJ = i;
+                newStr.add(matrix.get(matrix.get(0).size()-1 - j).get(i)); // cnt-clockwise
+                //newStr.add(matrix.get(matrix.get(0).size()-1 - i).get(matrix.get(0).size()-1 - j)); // reflect
+            }
+            rotatedMatrix.add(newStr);
+        }
+        return rotatedMatrix;
+    }
 
+    public static void printMatrix(List<List<String>> matrix) {
+        matrix.stream().forEach(
+                p -> {
+                    p.stream().forEach( f -> {
+                        System.out.printf("%s", f);;
+                    });
+                    System.out.println();
+                }
+        );
+    }
 
     static void clear() {
         inputList.clear();
